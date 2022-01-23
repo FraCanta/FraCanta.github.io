7726ce0ca43b897fd2ec1a4a651da13b8867827d
Francesca <fcantale14@gmail.com>
2022-01-23 15:50:11 +0100

    scrollUp

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,3 +153,29 @@ function cercaLibri() {
   function tornaIndietro() {
     window.location.reload();
   }
+
+  // scroll up button by id scrollUp in body and display after scroll down
+  // after scroll down the button is displayed
+  
+  // after scroll up the button is hidden
+  window.onscroll = function() {scrollFunction()};
+
+  //Get the button
+var mybutton = document.getElementById("scrollUp");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollUp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
